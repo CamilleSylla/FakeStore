4380e0f771c73ed69d5c5392dfa2970d3512dbd3
Camille Sylla <camillesylla.web@gmail.com>
2020-10-09 13:36:56 +0200

    first commit

---
+++ src/pages/product.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import data from '../App';
+
+
+class Product extends React.Component {
+constructor() {
+    super();
+    this.state = {
+        item : []
+    }
+}
+    componentDidMount() {
+        fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .then(item=>this.setState({item : item}))
+    }
+    renderData() {
+        return this.state.item.map((item, i) => {
+            const {id, title, price, category, image} = item;
+
+            return (
+                <div className="itemCont">
+                    <img src={image}/>
+                    <h2>{title}</h2>
+                    <p>{price}€</p>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        
+        return (
+            <div className="item">
+                {this.renderData()}
+            </div>
+        )
+    }
+
+}
+
+export default Product
